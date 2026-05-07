a589b1a2618a4814027daba02bcef087e5ae8788
7Kav <kamelayoung@googlemail.com>
2026-05-07 01:11:50 +0100

    random commit with broken code

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from fastapi import FastAPI
+import websocket-client
+from time import sleep
+import msgpack
+from os import getenv
+import asyncio
+import io
 
-app = FastAPI()
+from requests import api
+
+url = "https://beatgacha.com"
+
+def authenticate(username: str, password: str):
+    api_key = getenv("API_KEY")
+    if api_key is None:
+        raise Exception("API key not set")
+    else:
+        return api_key
+
+headers = {
+    "x-api-key": getenv("API_KEY"),
+    "x-user-id": getenv("USER_ID"),
+}
+
+ws = websocket.WebSocket()
+ws.connect(url, headers=headers)
+ws.send(url, headers=headers, "join_room", "pack_given")
+def CardPackNotify(ws, url, headers):
+    try:
+        while True:
+            sleep(30)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+            msg = ws.recv()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    except exit:
+        ws.close()
+        print("Connection closed")
+    except KeyboardInterrupt:
+        ws.close()
+        print("Connection closed")
+        print("Graceful shutdown")
+        exit()
