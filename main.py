f2534ac967183e753e7784050346966a3200ff8b
7Kav <kamelayoung@googlemail.com>
2026-09-17 00:25:34 +0100

    Finally got round to fixing this, fully works now.

--- main.py
+++ main.py
@@ -1,43 +1,85 @@
-import websocket-client
+import http
+import sys, subprocess
 from time import sleep
-import msgpack
 from os import getenv
-import asyncio
-import io
 
-from requests import api
 
-url = "https://beatgacha.com"
+# Set to "True" If you don't need the download
 
-def authenticate(username: str, password: str):
-    api_key = getenv("API_KEY")
-    if api_key is None:
-        raise Exception("API key not set")
-    else:
-        return api_key
+Skipdownload = True
+if Skipdownload == False:
+    try:
+        print("Attempting Dependency install")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    except subprocess.CalledProcessError:
+        print("ERROR")
+        print("Dependancy install failed. Please try again or run this command below")
+        print("pip install -m -r requirements.txt")
+        sys.exit()
+
+from dotenv import load_dotenv
+import requests
+load_dotenv()
+
+
+url = "https://beatgacha.com/api/auth/me"
+
+api_key = getenv("API_KEY")
+if api_key is None or api_key == "":
+    raise Exception("API key not set")
+
+userID = getenv("USER_ID")
 
 headers = {
-    "x-api-key": getenv("API_KEY"),
-    "x-user-id": getenv("USER_ID"),
+    "accept": "application/json",
+    "x-api-key": api_key,
+    "x-user-id": userID,
 }
 
-ws = websocket.WebSocket()
-ws.connect(url, headers=headers)
-ws.send(url, headers=headers, "join_room", "pack_given")
-def CardPackNotify(ws, url, headers):
+ShardCount = 0
+CardCount = 0
+
+
+def CardPackNotify():
     try:
-        while True:
-            sleep(30)
+        response = requests.get(url, headers=headers, timeout=15)
+        response.raise_for_status()
+        data = response.json()
 
+        username = data['user']['username']
+        ShardCount = f"{data['user']['card_shards']}"
+        CardCount = f"{data['user']['current_packs']} / {data['user']['max_packs']}"
 
-            msg = ws.recv()
+        return username, CardCount, ShardCount
+    except requests.exceptions.HTTPError == 404 as e:
+        if e.response.status_code == 404:
+            print("Data not found...?")
+        elif e.response.status_code == 401:
+            print("Access Denied.")
+            print("Did you enter both your API key and User ID?")
+        elif e.response.status_code == 405:
+            print("This shouldnt happen. Please report to the github repo.")
+        elif e.response.status_code == 500:
+            print("A server error occurred!")
+            print("Please check the BeatGacha Discord for any status updates!")
+        else:
+            print("A HTTP Exception occured when grabbing data!")
+            print(e)
+        return None, 0, 0
+    except requests.exceptions.Timeout as e:
+        print("Timeout Occured!")
+        print("No data was gathered")
+        return None, 0, 0
+    except requests.exceptions.ConnectionError as e:
+        print("Connection Error!")
+        print("Check your internet connection and try again")
+        return None, 0, 0
+    except Exception as e:
+        print(e)
+        return 0, 0
 
+username, CardCount, ShardCount = CardPackNotify()
 
-    except exit:
-        ws.close()
-        print("Connection closed")
-    except KeyboardInterrupt:
-        ws.close()
-        print("Connection closed")
-        print("Graceful shutdown")
-        exit()
+print(username)
+print(f"CardCount: {CardCount}")
+print(f"ShardCount: {ShardCount}")
